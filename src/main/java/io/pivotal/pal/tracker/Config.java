bbcc81a66b7c37ff5170375bdacd6a2e07a9fee6
Ryan Pando, robel gebregeorgis <Ryan.Pando@dish.com, robel.gebregeorgis>
2018-06-06 10:05:55 -0600

    Use mysql db instead of in memory hashmap

--- src/main/java/io/pivotal/pal/tracker/Config.java
+++ src/main/java/io/pivotal/pal/tracker/Config.java
@@ -4,15 +4,21 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class Config {
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
